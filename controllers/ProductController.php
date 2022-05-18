a14a9d685fdd05460e27c4daff1284c77e7b54cf
yassinexcv <yassinexcv188@gmail.com>
2022-05-18 12:11:14 +0000

    Products

---
+++ controllers/ProductController.php
@@ -0,0 +1,9 @@
+<?php
+
+
+class ProductController {
+    public function getAllProducts(){
+       $products = Product::getAll();
+         return $products;
+    }
+}
--- views/home.php
+++ views/home.php
@@ -1,1 +1,34 @@
-Home yassine 
+<?php
+$data = new ProductController();
+$products = $data->getAllProducts();
+?>
+
+
+<div class="container">
+    <div class="row my-5">
+        <div class="col-md-8">
+            <div class="row">
+                <?php
+                   if(count($products) > 0) :
+                ?>
+                <?php 
+                foreach($products as $product) :
+                 ?>
+                 <div class="card h-100 bg-white rounded">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $product['product_title']; ?></h5>
+                        <p class="card-text"><?php echo $product['product_description']; ?></p>
+                        <p class="card-text">$<?php echo $product['product_price']; ?></p>
+                        <a href="product.php?id=<?php echo $product['id']; ?>" class="btn btn-primary">View Product</a>
+                    </div>
+                 </div>
+                <?php
+                        endforeach;
+                ?>
+                <?php 
+                    endif;
+                ?>
+            </div>
+        </div>
+    </div>
+</div>
